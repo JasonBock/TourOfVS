62b5b1a8f19f318d719f5378eb1c2bb1b8ff9c2a
Jason Bock <jason@jasonbock.net>
2020-03-12 12:34:50 -0500

    Added a project to demonstrate debugging difference

--- Code/CodeSnippetsAndINotifyPropertyChanged/CodeSnippetsAndINotifyPropertyChanged/Changing.cs
+++ Code/CodeSnippetsAndINotifyPropertyChanged/CodeSnippetsAndINotifyPropertyChanged/Changing.cs
@@ -1,3 +1,59 @@
-﻿namespace CodeSnippetsAndINotifyPropertyChanged
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CodeSnippetsAndINotifyPropertyChanged
 {
+	public class Customer
+		: INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private uint age;
+
+		private Guid identifier;
+		private string name;
+
+		private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") =>
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+		public uint Age
+		{
+			get => this.age;
+			set
+			{
+				if (value != this.age)
+				{
+					this.age = value;
+					this.NotifyPropertyChanged();
+				}
+			}
+		}
+
+		public Guid Identifier
+		{
+			get => this.identifier;
+			set
+			{
+				if (value != this.identifier)
+				{
+					this.identifier = value;
+					this.NotifyPropertyChanged();
+				}
+			}
+		}
+
+		public string Name
+		{
+			get => this.name;
+			set
+			{
+				if (value != this.name)
+				{
+					this.name = value;
+					this.NotifyPropertyChanged();
+				}
+			}
+		}
+	}
 }
---
+++ Code/DebuggingBehavior/DebuggingBehavior/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DebuggingBehavior
+{
+	public static class Program
+	{
+		static void Main()
+		{
+			var tasks = new List<Task>();
+			var values = new int[Environment.ProcessorCount];
+			Array.Fill(values, 3);
+
+			for (var i = 0; i < Environment.ProcessorCount; i++)
+			{
+				tasks.Add(Task.Run(() => Console.Out.WriteLine($"{i} - {values[i]}")));
+			}
+
+			Task.WaitAll(tasks.ToArray());
+		}
+	}
+}
